c6d37d27bc507632e4e0e9ff629242a1a9e6190a
Esteban <esteban_fs@hotmail.com>
2016-07-29 14:47:04 -0500

    Conexion MongoDB

--- Admin.py
+++ Admin.py
@@ -1,16 +1,36 @@
-# from InvoiceXML import Files
+import csv
+
 from pymongo import MongoClient
+import flatdict
+import pandas as pd
 
 
-# A = Files()
 
 
 client = MongoClient()
 db = client.primer
 coll = db.dataset
 
-# cursor = db.primer.find()
-# for document in cursor:
-#     print(document)
+rename = db.primer.updateMany( {}, { "$rename": { "nmae": "name" } } )
 
-print(db.primer.count())
+cursor = db.primer.find({ "factura.infofactura.importetotal": { '$gt': '0' } }, {"factura.infotributaria.nombrecomercial":1,
+                                                                                 "factura.infofactura.fechaemision":1 ,
+                                                                                 "factura.infofactura.totalsinimpuestos": 1,
+                                                                                 "factura.infofactura.totalconimpuestos.totalimpuesto.valor":1,
+                                                                                 "factura.infofactura.propina": 1,
+                                                                                 "factura.infofactura.importetotal": 1,
+                                                                                  "_id": 0
+                                                                                 })
+
+
+with open('dict.csv', 'w') as csv_file:
+    fieldnames = ['nombrecomercial', 'fechaemision','totalsinimpuestos','impuesto','propina','total']
+    writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
+    for document in cursor:
+        flat = flatdict.FlatDict(document)
+        print(flat)
+        # for key, value in flat.items():
+        #     writer.writerow(value)
+
+
+    #print(db.primer.count())
